2ff5d17f5f44be90d4b5f589d606713690fcc8e7
Simon Nagl <simonnagl@aim.com>
2017-03-12 17:53:47 +0100

    Add maven.

---
+++ src/main/java/org/firehol/netdata/plugin/PluginDaemon.java
@@ -0,0 +1,13 @@
+package org.firehol.netdata.plugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class PluginDaemon 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
